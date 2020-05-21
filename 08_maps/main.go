f3935e5f4be08c81dafd5ffa7a3e1cab1b38752a
Houdini <nonsoamadi@aol.com>
2020-05-21 18:52:06 +0100

    up next range

--- 08_maps/main.go
+++ 08_maps/main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	// Create a map Assigning key values to map
+	emails := map[string]string{"John": "john@gmail.com", "teddy": "teddy@gmail.com", "nonso": "nonsoamadi@aol.com"}
 
-	fmt.Println("Hello world")
+	fmt.Println(emails)
+	fmt.Println(len(emails))
+
+	// Assigning new Values
+	emails["bryan"] = "bryan@yahoo.com"
+	fmt.Println(emails)
 }
